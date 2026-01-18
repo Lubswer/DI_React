90f24c6fc1e6eb86336453378d9bf78a2504c53f
lubswer <lubswer@hotmail.com>
2026-01-18 16:41:37 -0500

    Cambios en pagina principal, se hizo la pagina principal estatica con React

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
+import Home from "./pages/Home";
+
 export default function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900">
-      <h1 className="text-4xl font-bold text-emerald-400">
-        Tailwind v4 funcionando 
-      </h1>
-    </div>
-  );
+  return <Home />;
 }
